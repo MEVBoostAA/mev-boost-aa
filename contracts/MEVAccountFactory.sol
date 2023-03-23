0820096dfebbc79e426d8ba9999235f6b65bea54
doublespending <outofgas@qq.com>
2023-03-23 11:50:52 +0800

    refactor account factory

--- contracts/MEVAccountFactory.sol
+++ contracts/MEVAccountFactory.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/utils/Create2.sol";
-import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-
-import "./MEVAccount.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {IEntryPoint} from "./interfaces/IEntryPoint.sol";
+import {MEVAccount} from "./MEVAccount.sol";
 
 contract MEVAccountFactory {
     MEVAccount public immutable accountImplementation;
