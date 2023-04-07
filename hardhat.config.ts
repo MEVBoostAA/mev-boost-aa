404a4c4b71ab83aa693dec99d270be09488b0803
doublespending <outofgas@qq.com>
2023-04-07 20:37:01 +0800

    init hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-foundry";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+export default config;
